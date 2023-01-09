12a2c6cf0a463d76e8e2c2404bf7939ed2524224
amir <amir.salimi1810@gmail.com>
2023-01-09 11:36:00 +0100

    before_db

--- Backend/Reserve/Office/models.py
+++ Backend/Reserve/Office/models.py
@@ -1,27 +1,23 @@
 from django.db import models
 
+# other libraries
+from datetime import date
+
 # Create your models here.
 
-class Floor1DB(models.Model):
+class FloorsDB(models.Model):
+
+    status_of_floors = (
+        (1, 'First floor'), (2, 'Second floor'), (3, 'Third floor')
+    )
+
+    floor = models.IntegerField(choices=status_of_floors)
     officeName = models.CharField(max_length=35)
     equipment = models.BooleanField(default=True)
     capacity = models.IntegerField()
 
     def __str__(self):
-        return f'First floor, number {self.officeName}'
+        return f'{self.floor}, number {self.officeName}'
 
-class Floor2DB(models.Model):
-    officeName = models.CharField(max_length=35)
-    equipment = models.BooleanField(default=True)
-    capacity = models.IntegerField()
 
-    def __str__(self):
-        return f'Second floor, number {self.officeName}'
 
-class Floor3DB(models.Model):
-    officeName = models.CharField(max_length=35)
-    equipment = models.BooleanField(default=True)
-    capacity = models.IntegerField()
-
-    def __str__(self):
-        return f'Third floor, number {self.officeName}'
--- Backend/Reserve/Office/views.py
+++ Backend/Reserve/Office/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+# old version
+# def home_page(request):
+#     return render(request, "Office/test.html", {})
+
+
+# new version
+class HomePageView(TemplateView):
+    template_name = 'Office/test.html'
